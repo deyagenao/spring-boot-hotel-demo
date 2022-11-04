aed76f6dd84d779f36e2bcb9d1536dd870f304e0
Deyanira Genao <deyagenao@gmail.com>
2022-11-04 19:15:37 -0400

    set up app, create POJOs and repositories, import schema and date

---
+++ src/main/java/com/deyagenao/data/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.deyagenao.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long>{
+
+}
---
+++ src/main/java/com/deyagenao/util/AppStartupEvent.java
@@ -0,0 +1,38 @@
+package com.deyagenao.util;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import com.deyagenao.data.Guest;
+import com.deyagenao.data.GuestRepository;
+import com.deyagenao.data.Reservation;
+import com.deyagenao.data.ReservationRepository;
+import com.deyagenao.data.Room;
+import com.deyagenao.data.RoomRepository;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent>{
+
+	private final RoomRepository roomRepository;
+	private final GuestRepository guestRepository;
+	private final ReservationRepository reservationRepository;
+	
+	public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository,
+			ReservationRepository reservationRepository) {
+		this.roomRepository = roomRepository;
+		this.guestRepository = guestRepository;
+		this.reservationRepository = reservationRepository;
+	}
+	
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		Iterable<Room> rooms = this.roomRepository.findAll();
+		rooms.forEach(System.out::println);
+		Iterable<Guest> guests = this.guestRepository.findAll();
+		guests.forEach(System.out::println);
+		Iterable<Reservation> reservations = this.reservationRepository.findAll();
+		reservations.forEach(System.out::println);
+	}
+
+}
